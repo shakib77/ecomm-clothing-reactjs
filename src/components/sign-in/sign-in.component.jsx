c43f5be186d5a5355ab53312d0f224eab3b99bac
shakib77 <shakibshaon77@gmail.com>
2020-06-02 12:20:30 +0600

    added redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -8,67 +8,67 @@ import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password:''
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    handleSubmit = async event => {
-        event.preventDefault();
-
-        const { email, password } = this.state;
-
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: '' });
-        }  catch (error) {
-            console.log(error);
-        }
+    this.state = {
+      email: '',
+      password: ''
     };
+  }
 
-    handleChange = event => {
-        const { value, name } = event.target;
+  handleSubmit = async event => {
+    event.preventDefault();
 
-        this.setState({ [name]: value })
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  handleChange = event => {
+    const { value, name } = event.target;
 
-    render() {
-        return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+    this.setState({ [name]: value });
+  };
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                    name='email' 
-                    type='email' 
-                    value={this.state.email} 
-                    handleChange={this.handleChange}
-                    label='email'
-                    required />
-                    
-                    <FormInput 
-                    name='password' 
-                    type='password' 
-                    value={this.state.password} 
-                    handleChange={this.handleChange}
-                    label='password'
-                    required 
-                    />
-                    <div className='buttons'>
-                        <CustomButton type='submit' > Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                            Sign in with Google
-                        </CustomButton>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,49 +3,47 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyA4NYLfxzT5SfVFyhkY3QbFPCyh--gacNg",
-    authDomain: "ecomm-clothing-reactjs-db.firebaseapp.com",
-    databaseURL: "https://ecomm-clothing-reactjs-db.firebaseio.com",
-    projectId: "ecomm-clothing-reactjs-db",
-    storageBucket: "ecomm-clothing-reactjs-db.appspot.com",
-    messagingSenderId: "676316834140",
-    appId: "1:676316834140:web:0d536b5b604a29154bf999",
-    measurementId: "G-R3N66SV00L"
-  };
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
+};
 
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+firebase.initializeApp(config);
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-    const snapShort = await userRef.get();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    if (!snapShort.exists) {
-      const { displayName, email } = userAuth;
-      const createdAt = new Date();
+  const snapShot = await userRef.get();
 
-      try {
-        await userRef.set({
-          displayName,
-          email,
-          createdAt,
-          ...additionalData
-        })
-      } catch (error) {
-        console.log('error creating user', error.message);
-      }
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
+  }
 
-    return userRef;
-  };
+  return userRef;
+};
 
-  firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
-  export default firebase;
+export default firebase;
