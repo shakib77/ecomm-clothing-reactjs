b4caa3a391eb4d4aa478e304f88a60d743631d55
shakib77 <shakibshaon77@gmail.com>
2020-08-26 00:41:32 +0600

    add directory state into redux

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -20,12 +20,13 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   );
 
   if (existingCartItem.quantity === 1) {
-    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
   }
 
-  return cartItems.map(cartItem =>
-    cartItem.id === cartItemToRemove.id
-      ? { ...cartItem, quantity: cartItem.quantity - 1 }
-      : cartItem
+  return cartItems.map(
+    cartItem =>
+    cartItem.id === cartItemToRemove.id ?
+    { ...cartItem, quantity: cartItem.quantity - 1 }
+    : cartItem
   );
 };
